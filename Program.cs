3af8b4727b29b970193a7116922ab793f2850110
Argollo <rafaelargollo22@hotmail.com>
2022-07-19 17:08:34 -0300

    Testando leitura de planilhas Excel

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using ClosedXML.Excel;
+using System.Linq;
+namespace TesteExcel
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var xls = new XLWorkbook(@"C:\Users\rafae\Documents\dívidas mensais.xlsx");
+            var planilha = xls.Worksheets.First();
+            var totalLinhas = planilha.Rows().Count();
+            var cartao = double.Parse(planilha.Cell("D5").Value.ToString());
+            var seguro = double.Parse(planilha.Cell("D6").Value.ToString());
+            Console.WriteLine($"{cartao} e {seguro}");
+        }
+    }
+}
